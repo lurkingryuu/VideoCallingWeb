209bcdf73b77769f212309011e9187f93c6201a3
Karthikeya Y <yelisettikarthik0@gmail.com>
2022-02-16 20:56:13 +0530

    changed the host for peer

--- public/script.js
+++ public/script.js
@@ -1,7 +1,8 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: '/',
+  host: 'video-chat-lurkingryuu.herokuapp.com',
+  secure: true,
   port: '3001'
 })
 const myVideo = document.createElement('video')
